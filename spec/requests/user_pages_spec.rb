6a567e4e0bc989a6a153b4558b17bf52c96d6915
John A. Martin <john.a.martin.ak@gmail.com>
2013-12-03 14:10:03 -0900

    Add gravatar size, handle testing errors, change flash test

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -22,6 +22,12 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+      describe "after submission" do
+        before { click_button submit }
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+        it { should_not have_content('Password digest') }
+      end
     end
     describe "with valid information" do
       before do
@@ -32,6 +38,13 @@ describe "User pages" do
       end
       it "should check a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('h1', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
