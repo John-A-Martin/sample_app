0aa0a32a6bd70741650e4204c4fb6dd52a69169b
John A. Martin <john.a.martin.ak@gmail.com>
2013-12-11 15:41:42 -0900

    add unfollowing

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,6 +41,9 @@ class User < ActiveRecord::Base
   def follow!(other_user)
     relationships.create!(followed_id: other_user.id)
   end
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
+  end
 
   private
     def create_remember_token
